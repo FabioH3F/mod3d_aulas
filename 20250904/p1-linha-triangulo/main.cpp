ef05679bca43032e38a658335edf012004f448e5
Fabio Henrique Esprovieri FIlho <jackesprovieri@gmail.com>
2025-09-04 20:30:25 -0300

    linhas-fixas

--- 20250904/p1-linha-triangulo/main.cpp
+++ 20250904/p1-linha-triangulo/main.cpp
@@ -9,8 +9,16 @@
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
     //define area de enquadramento da cena
     gluOrtho2D(-3, 3, -3, 3);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glLineWidth(10);
     glBegin(GL_LINES);
         glVertex2f(-2.0f, 0.0f);
         glVertex2f(2.0f, 0.0f);
@@ -23,10 +31,10 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
-
     //as letras maiuscula sao conatntes
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("GLUT Shapes");
+    glClearColor(0.4, 0, 1, 0);
     glutDisplayFunc(display);
     glutMainLoop();
     return EXIT_SUCCESS;
