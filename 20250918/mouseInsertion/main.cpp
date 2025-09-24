64d5a34aef0abfa5c82ebf6f9819e051076b2bf8
FabioH3F <jackesprovieri@gmail.com>
2025-09-23 22:00:13 -0300

    addMouse

--- 20250918/mouseInsertion/main.cpp
+++ 20250918/mouseInsertion/main.cpp
@@ -1,8 +1,3 @@
-/*
- * by: fabio
- * proj. tarefa1
- */
-
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <stdlib.h>
@@ -11,33 +6,55 @@
 float r, g, b, x, y;
 bool check=true;
 
-void mouse (int button, int state, int mousex, int y)
+void mouse (int button, int state, int mousex, int mousey)
+{
+    if (button==GLUT_LEFT_BUTTON)
+    {
+        check=true;
+        x = mousex;
+        y = 480-mousey;
+        r=(rand()%10)/10.0;
+        g=(rand()%10)/10.0;
+        b=(rand()%10)/10.0;
+    }
+    else if(button==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
+    {
+        glClearColor(1, 1, 1, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        check = false;
+    }
+    glutPostRedisplay();
+}
+
 
 static void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    //define a matriz de transformação da câmera
+    glColor3f(r,g,b);
+    glPointSize(50);
     glMatrixMode(GL_PROJECTION);
-    //seta a matrix identidade para a câmera
     glLoadIdentity();
-    //define área de enquadramento da cena
-    gluOrtho2D(-3, 3, -3, 3);
-
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+    if(check)
+    {
+        glBegin(GL_POINTS);
+        glVertex2i(x,y);
+        glEnd();
+    }
     glFlush();
 
 }
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
-    //as letras maiuscula sao conatntes
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("GLUT Shapes");
-    glClearColor(0, 0, 0, 0);
+    glClearColor(1, 1, 1, 0);
     glutDisplayFunc(display);
+    glutMouseFunc(mouse);
     glutMainLoop();
     //return EXIT_SUCCESS;
 }
--- tarefa1nome/main.cpp
+++ tarefa1nome/main.cpp
@@ -2,14 +2,59 @@
  * by: fabio
  * proj. tarefa1
  */
-
+#include<stdio.h>
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <iostream>
 
 GLfloat escala = 0.3f;
-GLfloat rotaciona = 0.0f;
-GLfloat mexex = 0.0f;
-GLfloat mexey = 0.0f;
+GLfloat rotacao = 0.0f;
+GLfloat x = 0.0f;
+GLfloat y = 0.0f;
+GLfloat min_escala = 0.1f;
+
+void nome()
+{
+    glLineWidth(4);
+    glBegin(GL_LINES);
+    // F
+    glVertex2f(-8, -4); glVertex2f(-8, 4);
+    glVertex2f(-8, 4);  glVertex2f(-6, 4);
+    glVertex2f(-8, 0);  glVertex2f(-6.5f, 0);
+
+    // A
+    glVertex2f(-4.5f, -4); glVertex2f(-3, 4);
+    glVertex2f(-3, 4);     glVertex2f(-1.5f, -4);
+    glVertex2f(-4, 0);     glVertex2f(-2, 0);
+
+    // B
+    glVertex2f(0, -4);  glVertex2f(0, 4);
+    glVertex2f(0, 4);   glVertex2f(1.5f, 4);
+    glVertex2f(1.5f, 4); glVertex2f(1.5f, 1);
+    glVertex2f(1.5f, 1); glVertex2f(0, 1);
+    glVertex2f(0, 0);   glVertex2f(1.8f, 0);
+    glVertex2f(1.8f, 0); glVertex2f(1.8f, -4);
+    glVertex2f(1.8f, -4); glVertex2f(0, -4);
+
+    // I
+    glVertex2f(3, -4);  glVertex2f(3, 4);
+    glVertex2f(2.5f, 4); glVertex2f(3.5f, 4);
+    glVertex2f(2.5f, -4); glVertex2f(3.5f, -4);
+
+    // O
+    glVertex2f(5.5f, 4); glVertex2f(5.5f, -4);
+    glVertex2f(6.5f, 4); glVertex2f(6.5f, -4);
+    glVertex2f(5.5f, 4); glVertex2f(6.5f, 4);
+    glVertex2f(5.5f, -4); glVertex2f(6.5f, -4);
+    glEnd();
+}
+
+int dentroTela(GLfloat novo_x, GLfloat novo_y) {
+    GLfloat limite = 8.0f * escala;
+    return (novo_x - limite > -10 && novo_x + limite < 10 &&
+            novo_y - limite > -7 && novo_y + limite < 7);
+}
+
 
 //função para interação de teclado
 void listeningKey(unsigned char tecla, GLint col, GLint linha)
@@ -18,27 +63,16 @@ void listeningKey(unsigned char tecla, GLint col, GLint linha)
     switch(tecla)
     {
 
-        case '+':if (escala < 0.35f ){escala += 0.02f;}
-                break;
-        case '-':if (escala > 0.1f){escala=escala-0.05f;}
-                break;
-        case 'q':rotaciona=rotaciona+1.0f;
-                break;
-        case 'e':rotaciona=rotaciona-1.0f;
-                break;
-
-        case 'd':mexex=mexex+1.0f;
-                break;
-        case 'a':mexex=mexex-1.05f;
-                break;
-
-        case 'w':mexey=mexey+1.0f;
-                break;
-        case 's':mexey=mexey-1.0f;
-                break;
+        case '+': if (escala < 1.0f) escala += 0.05f; break;
+        case '-': if (escala > min_escala) escala -= 0.05f; break;
+        case 'q': rotacao += 5.0f; break;
+        case 'e': rotacao -= 5.0f; break;
+        case 'a': if (dentroTela(x - 0.2f, y)) x -= 0.2f; break;
+        case 'd': if (dentroTela(x + 0.2f, y)) x += 0.2f; break;
+        case 'w': if (dentroTela(x, y + 0.2f)) y += 0.2f; break;
+        case 's': if (dentroTela(x, y - 0.2f)) y -= 0.2f; break;
     }
     glutPostRedisplay();
-    //display();
 }
 
 static void display(void)
@@ -50,67 +84,25 @@ static void display(void)
     //seta a matrix identidade para a câmera
     glLoadIdentity();
     //define área de enquadramento da cena
-    gluOrtho2D(-3.0, 3.0, -3.0, 3.0);
+    gluOrtho2D(-10, 10, -7, 7);
 
     //move o objeto
-    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(x, y, 0.0f);
 
     /*rotacina o objeto
     ordem dos parmetros ANG, X, Y, Z*/
-    glRotatef(rotaciona, 0.0f, 0.0f, 1.0f);
+    glRotatef(rotacao, 0.0f, 0.0f, 1.0f);
 
-
-    glScalef(escala, escala, 0);
-
-    glTranslatef(mexex, 0.0f, 0.0f);
-
-    glTranslatef(0.0f, mexey, 0.0f);
-
+    glScalef(escala, escala, 1.0f);
 
     //define a matrix de transformação dos modelos "não muda o objeto de lugar"
     glMatrixMode(GL_MODELVIEW);
     //seta a matrix identidade para o modelos
     glLoadIdentity();
 
-    glLineWidth(10);
-    glBegin(GL_LINES);
-        //H
-        glVertex2f(-8.0f, 8.0f);
-        glVertex2f(-8.0f, -8.0f);
-
-        glVertex2f(-8.0f, 5.0f);
-        glVertex2f(-6.0f, 5.0f);
-
-        glVertex2f(-6.0f, 8.0f);
-        glVertex2f(-6.0f, -8.0f);
-
-        //E
-        glVertex2f(-1.0f, 8.0f);
-        glVertex2f(-1.0f, -8.0f);
-
-        glVertex2f(-1.0f, 8.0f);
-        glVertex2f(1.0f, 8.0f);
-
-        glVertex2f(-1.0f, 5.0f);
-        glVertex2f(1.0f, 5.0f);
-
-        glVertex2f(-1.0f, -8.0f);
-        glVertex2f(1.0f, -8.0f);
-
-        //F
-        glVertex2f(6.0f, 8.0f);
-        glVertex2f(6.0f, -8.0f);
-
-        glVertex2f(6.0f, 8.0f);
-        glVertex2f(8.0f, 8.0f);
-
-        glVertex2f(6.0f, 5.0f);
-        glVertex2f(8.0f, 5.0f);
-    glEnd();
-
+    nome();
 
     glFlush();
-
 
 }
 
@@ -119,7 +111,7 @@ static void display(void)
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
+    glutInitWindowSize(800, 600);
     glutInitWindowPosition(10,10);
     //as letras maiuscula sao conatntes
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
