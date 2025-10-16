54bc584399a968910f4f285638456d302bd31df7
FabioH3F <jackesprovieri@gmail.com>
2025-10-16 19:54:11 -0300

    2025161002

---
+++ 20251016/p1-circulo/p1-circulo/main.cpp
@@ -0,0 +1,60 @@
+#include<GL/glut.h>
+#include<stdio.h>
+// a math exige em radianos os angulos
+#include <math.h>
+#define PI 3.1415926535898
+GLfloat escala = 1;
+int i = 0;
+void desenha(void)
+{
+    glClear( GL_COLOR_BUFFER_BIT );
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-6, 6, -6, 6);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glColor3f(1, 0, 0);
+    glLineWidth(6);
+    float ang, x, y;
+    glBegin(GL_LINE_LOOP);
+    for (i = 0; i < 360; i++)
+        // a math exige em radianos os angulos, entao como definimos em graus o i no for, precisamos passar esses angulos em radianos
+    {
+//angle = 2*PI*i/circle_points;
+// passamos o angulo para radianos
+        ang = (i * PI) / 180.0;
+        // o valor "0" eh para posicionar o elemento "eh o centro de" , o *4 eh para multipilicar o tamanho do circulo
+        x = 0 + (cos(ang) * escala);
+        y = 0 + (sin(ang) * escala);
+        glVertex2f(x, y);
+    }
+    glEnd();
+    glFlush();
+}
+void listeningKey (unsigned char tecla,
+                   GLint x, GLint y)
+{
+    switch(tecla)
+    {
+    case '+':
+        escala=escala+0.25;
+        break;
+    case '-':
+        escala=escala-0.25;
+        break;
+    }
+    desenha();
+}
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(300,100);
+    glutCreateWindow("Ola Glut");
+    glutKeyboardFunc(listeningKey);
+    glutDisplayFunc(desenha);
+    glClearColor( 0, 0, 1, 0);
+    glutMainLoop();
+    return 0;
+}
